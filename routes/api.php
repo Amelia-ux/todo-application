fcf5fbc5c37f3ebaaabb38a25556a85fd7383294
amelia <meliamel.9977@gmail.com>
2022-06-22 18:30:10 +0700

    Praktikum 1: Fitur logout

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(['suth:sanctum'])->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
